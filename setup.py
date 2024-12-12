d63f26af31a28329489dc2d06e5d60634a042a01
mane <53539004+tommhe14@users.noreply.github.com>
2024-12-12 14:02:19 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,46 @@
-try: 
-   from setuptools import setup 
-except ImportError: 
-   from distutils.core import setup 
+import os, sys
+from setuptools import setup, find_packages
 
-import sys, os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'gogokit'))
+package_dir = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(package_dir, 'gogokit'))
+
 from config import __version__, __pypi_packagename__
 
+with open(os.path.join(package_dir, 'LICENSE.txt')) as license_file:
+    license_text = license_file.read()
+
+with open(os.path.join(package_dir, 'README.md')) as readme_file:
+    long_description = readme_file.read()
+
 setup(
-  name=__pypi_packagename__,
-  version= __version__,
-  author='Viagogo',
-  author_email='api@viagogo.com',
-  packages=['gogokit'],
-  scripts=[],
-  url='https://github.com/viagogo/gogokit.py',
-  license=open('LICENSE.txt').read(),
-  description='GogoKit is a lightweight, viagogo API client library for Python.',
-  long_description=open('README.md').read(),
-  install_requires=['requests>=2.7.0', 'uritemplate>=0.6', 'iso8601>=0.1.10', 'six>=1.9.0'],
-  classifiers=[
-    'Intended Audience :: Developers',
-    'Natural Language :: English',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: Implementation :: PyPy',
-    'Topic :: Software Development :: Libraries :: Python Modules'
-  ],
-  keywords=['viagogo', 'rest', 'sdk', 'api']
+    name=__pypi_packagename__,
+    version=__version__,
+    author='Mane',
+    author_email='theckley@yahoo.co.yk',
+    packages=find_packages(include=['gogokit', 'gogokit.*']),
+    url='https://github.com/tommhe14/better-gogokit.py',
+    license=license_text,
+    description='A lightweight, viagogo API client library for Python.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'requests>=2.7.0',
+        'uritemplate>=0.6',
+        'iso8601>=0.1.10',
+        'six>=1.9.0',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords=['viagogo', 'rest', 'sdk', 'api'],
+    python_requires='>=3.7',
 )
